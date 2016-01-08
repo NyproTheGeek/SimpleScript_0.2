9c746a72617f8ac639072ce7db9dcf0139466dc4
Nypro <steveakinyemi@gmail.com>
2016-01-09 00:14:29 +0100

    Added  the src folder

---
+++ src/com/simplescript/Main.java
@@ -0,0 +1,20 @@
+package com.simplescript;
+
+
+import java.util.Scanner;
+
+// 08/01/16 Nypro
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.print("Enter a valid file path: ");
+
+        Scanner filePath = new Scanner(System.in); // Scanner object used to get filePath from console
+
+        String input = filePath.nextLine();
+
+	    Parser.parseSource(input); // The filePath leads to the source file that the Parser class parses
+
+        // This is a GitHub test
+    }
+}
---
+++ src/com/simplescript/Parser.java
@@ -0,0 +1,13 @@
+package com.simplescript;
+
+/**
+ * Created by Nypro on 1/8/2016.
+ */
+public class Parser
+{
+
+	public static void parseSource(String filePath)
+	{
+
+	}
+}
