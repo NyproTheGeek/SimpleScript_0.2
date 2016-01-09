5dbe61db3ca11a182ed5c377f24d594680c06f1a
Nypro <steveakinyemi@gmail.com>
2016-01-09 11:43:52 +0100

    Added a file reader

--- src/com/simplescript/Parser.java
+++ src/com/simplescript/Parser.java
@@ -1,13 +1,28 @@
 package com.simplescript;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 /**
  * Created by Nypro on 1/8/2016.
  */
 public class Parser
 {
 
-	public static void parseSource(String filePath)
+	public static void parseSource(String filePathString) throws IOException
 	{
-
+		Path filePath = Paths.get(filePathString);
+		try (Scanner inFile = new Scanner (filePath, "UTF-8"))
+		{
+			//
+		}
+		catch(FileNotFoundException fnfe)
+		{
+			System.err.println("File not found dude!\nCheck under the bed");
+			fnfe.printStackTrace();
+		}
 	}
 }
