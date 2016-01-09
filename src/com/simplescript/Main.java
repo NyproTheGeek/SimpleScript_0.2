62ddddc49b7819263c5a38d115607c230e697837
Nypro <steveakinyemi@gmail.com>
2016-01-09 01:35:09 +0100

    No meaningful change

--- src/com/simplescript/Main.java
+++ src/com/simplescript/Main.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 // 08/01/16 Nypro
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.print("Enter a valid file path: ");
+	public static void main(String[] args)
+	{
+		System.out.print("Enter a valid file path: ");
 
-        Scanner filePath = new Scanner(System.in); // Scanner object used to get filePath from console
+		Scanner filePath = new Scanner(System.in); // Scanner object used to get filePath from console
 
-        String input = filePath.nextLine();
+		String input = filePath.nextLine();
 
-	    Parser.parseSource(input); // The filePath leads to the source file that the Parser class parses
+		Parser.parseSource(input); // The filePath leads to the source file that the Parser class parses
 
-        //
+		// Hello world
     }
 }
